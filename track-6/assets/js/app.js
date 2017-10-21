90f620d6934ee8f2aa4691733dfcb3c80be61f24
Verity <Verity@niuruowens-MacBook-Pro.local>
2017-10-21 15:00:51 -0400

    track63

--- track-6/assets/js/app.js
+++ track-6/assets/js/app.js
@@ -5,15 +5,18 @@ $('.choicebutton').click(function() {
       $('.lightbox').removeClass('hide1');
       $('.mainpage').addClass('hide');
       $('audio')[0].play();
+      $('.end').addClass('hide');
     } 
 
      if (button_name == 'read') {
       $('.lightbox').removeClass('hide2');
       $('.mainpage').addClass('hide');
+      $('.end').addClass('hide');
     } 
 	if (button_name == 'recall') {
       $('.lightbox').removeClass('hide3');
       $('.mainpage').addClass('hide');
+      $('.end').addClass('hide');
     } 
 
 });
@@ -24,15 +27,18 @@ $('.sound').click(function(){
   $('.lightbox').addClass('hide1');
   $('audio')[0].pause();
   $('.mainpage').removeClass('hide');
+  $('.end').removeClass('hide');
 });
 
 $('.back').click(function(){
   $('.lightbox').addClass('hide2');
   $('.mainpage').removeClass('hide');
+  $('.end').removeClass('hide');
 });
 
 $('.finish').click(function(){
   $('.lightbox').addClass('hide3');
   $('.mainpage').removeClass('hide');
+  $('.end').removeClass('hide');
 });
 
