2b9ed1ea69102ec8ac5a7b182374dcc51292c7db
Verity <Verity@niuruowens-MacBook-Pro.local>
2017-11-29 14:30:02 -0500

    track1,2

--- album/final-2/assets/js/app.js
+++ album/final-2/assets/js/app.js
@@ -1,3 +1,6 @@
+$('.front').click(function(){
+	$('.front').addClass('hide')
+});
 $('.question1').click(function(){
 	$('.question2').removeClass('hide')
 });
@@ -6,4 +9,16 @@ $('.question2').click(function(){
 });
 $('.answer1').click(function(){
 	$('.answer2').removeClass('hide')
+});
+$('.answer2').click(function(){
+	$('.answer3').removeClass('hide')
+});
+$('.answer3').click(function(){
+	$('.answer4').removeClass('hide')
+});
+$('.answer4').click(function(){
+	$('.answer5').removeClass('hide')
+});
+$('.answer5').click(function(){
+	$('.answer6').removeClass('hide')
 });
